2a11ca9a7102f4d99dacbf7577099fef6c31cf48
dongatuna <ngatuna05@gmail.com>
2018-07-06 16:44:58 -0700

    add product model and install mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+
+const Product = require("../models/product");
+
 router.get('/', (req, res, next)=>{
     res.status(200).json({
         message: 'Handling GET request to /products'
@@ -9,10 +13,16 @@ router.get('/', (req, res, next)=>{
 
 router.post('/', (req, res, next)=>{
 
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
-    }
+        price:req.body.price
+    });
+
+    product.save().then(result=>{
+        console.log(result)
+    })
+    .catch(err => console.log(err));
     res.status(201).json({
         message: 'Handling POST request to /products',
         createdProduct: product
--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+//connect to db
+mongoose.connect('mongodb://node-rest-shop:'+process.env.MONGO_ATLAS_PW+
+'@node-rest-shop-shard-00-00-usfax.mongodb.net:27017,node-rest-shop-shard-00-01-usfax.mongodb.net:27017,node-rest-shop-shard-00-02-usfax.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true',
+{ useNewUrlParser: true });
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
