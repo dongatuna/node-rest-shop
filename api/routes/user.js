17c0bf3b07888f9b2b49f08bd4b9f864de0eccbb
dongatuna <ngatuna05@gmail.com>
2018-07-12 08:03:15 -0700

    add delete in user route

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,38 +6,66 @@ const bcrypt = require("bcryptjs");
 //import the User model
 const User = require("../models/user");
 
-router.post('/signup', (req, res, next)=>{
-    bcrypt.hash(req.body.password, 10, (err, hash)=>{
-
-        if(err){
-            return res.status(500).json({
-                error:err
+router.post('/signup', (req, res, next) => {
+    //first find if the email is taken
+    User.find({email: req.body.email})
+    .exec()
+    .then(user =>{
+        //if a user exists, return (exit) info to user
+        if(user.length>=1){
+            return res.status(422).json({
+                message: "The email used is taken"
             });
-        }else{
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-            });
-
-            user.save()
-            .then(result =>{
-                console.log(result);
-                res.status(201).json({
-                    message: "User created"
+        } else {
+            bcrypt.hash(req.body.email, 10, (err, hash)=>{
+                //create a new user
+                const user = new User({
+                    _id: new mongoose.Types.ObjectId(),
+                    email: req.body.email,
+                    password: hash
                 });
-            })
-            .catch(err=>{
+
+                //save the user
+                user
+                .save()
+                .then(result =>{
+                    console.log(result);
+                    res.status(201).json({
+                        message:"User created"
+                    });
+                })
+                .catch(err=>{
                     console.log(err);
+
                     res.status(500).json({
                         error:err
                     });
-                }
-                
-            );
+                });
+            })
         }
-    });
-   
+    })
+    
 });
 
+router.delete("/:userId", (req, res, next) => {
+    User.remove({_id: req.params.userId})
+    .exec()
+    .then(results =>{
+
+        res.status(200).json({
+            message: "The user has been deleted",
+
+            request:{                
+                type: "POST",
+                link: 'http://localhost:3000/user/signup',
+                data: {email: "String", password: "String"}
+            }
+        });
+    })
+    .catch(err =>{
+        res.status(500).json({
+            error:err
+        });
+    });
+});
 module.exports = router;
--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use((req, res, next)=>{
 //Routes to handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
-app.use('/user', orderRoutes);
+app.use('/user', userRoutes);
 
 //Error handling
 app.use((req, res, next)=>{
